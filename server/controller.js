b45476ac2f4a6e361a0e54cb24b37b50d2d975d6
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-02 16:16:47 -0400

    Finished all functions

--- server/controller.js
+++ server/controller.js
@@ -1,11 +1,40 @@
 const houses = require('./db.json');
-let houseNumber = 4;
+//let houseNumber = 4;
 
 module.exports = {
     getHouses: (req, res) => {
         res.status(200).send(houses);
+    },
+
+    deleteHouse: (req, res) => {
+        let houseIndex = houses.findIndex((house) => house.id === +req.params.id);
+        houses.splice(houseIndex, 1);
+        res.status(200).send(houses);
+    },
+
+    createHouse: (req, res) => {
+        let {address, price, imageURL} = req.body;
+
+        let newHouse = {
+            id: houses.length + 1,
+            address,
+            price,
+            imageURL
+        };
+
+        houses.push(newHouse);
+        res.status(200).send(houses);
+    },
+
+    updateHouse: (req, res) => {
+        let requestID = req.params.id;
+        let houseIndex = houses.findIndex((house) => house.id === +requestID);
+        if (req.body.type === 'plus') {
+            houses[houseIndex].price += 10000;
+        } else if (req.body.type === 'minus') {
+            houses[houseIndex].price -= 10000;
+        }
+
+        res.status(200).send(houses);
     }
-    //deleteHouse:
-    //createHouse:
-    //updateHouse:
 }
--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/api/houses', ctrl.getHouses);
-//app.post('/api/houses', ctrl.createHouse);
-//app.delete('/api/houses/:id', ctrl.deleteHouse);
-//app.put('/api/houses/:id', ctrl.updateHouse);
+app.post('/api/houses', ctrl.createHouse);
+app.delete('/api/houses/:id', ctrl.deleteHouse);
+app.put('/api/houses/:id', ctrl.updateHouse);
 
 app.listen(4004, () => console.log('Server is running on port 4004'));
